3a80af5d0cdf3479d4f04872252dadba002c3b7f
Marit van Helden <167195477+DenMots@users.noreply.github.com>
2024-05-13 23:36:15 +0200

    v0.0.6
    Add index

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import Head from '@/components/meta/Head';
+
+import { getBaseUrl } from '@/helpers/url';
+
+import IndexContents from '@/contents/index';
+
+function Index() {
+  return (
+    <>
+      <Head
+        title="Marit van Helden · Mediavormgever · Growth hacker · UI/UX · Developer "
+        description="An online portfolio featuring a showcase of my projects and some thoughts as a Front-End Developer who loves intuitive, clean and modern UI design."
+        ogImage={`${getBaseUrl()}/assets/images/og-image.png`}
+        overrideTitle
+      />
+      <IndexContents />
+    </>
+  );
+}
+
+export default Index;
